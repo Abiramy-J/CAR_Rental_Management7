3e5a0a6fc000775fc4b571e3a77ee01df3cea3f0
BOSS <kanga8482@gmail.com>
2025-08-26 21:53:58 +0530

    login

---
+++ CAR/Controllers/AccountController.cs
@@ -0,0 +1,76 @@
+﻿using CAR.Data;
+using CAR.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CAR.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Account/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: /Account/Login
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Username == username && u.Password == password);
+            if (user != null)
+            {
+                HttpContext.Session.SetInt32("UserID", user.UserID);
+                HttpContext.Session.SetString("Role", user.Role);
+
+                if (user.Role == "Admin")
+                    return RedirectToAction("Dashboard", "Admin");
+                else
+                    return RedirectToAction("Dashboard", "Customer");
+            }
+
+            ViewBag.Error = "Invalid username or password!";
+            return View();
+        }
+
+        // GET: /Account/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: /Account/Register
+        [HttpPost]
+        public IActionResult Register(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_context.Users.Any(u => u.Username == model.Username))
+                {
+                    ViewBag.Error = "Username already exists!";
+                    return View();
+                }
+
+                _context.Users.Add(model);
+                _context.SaveChanges();
+
+                return RedirectToAction("Login");
+            }
+            return View(model);
+        }
+
+        // Logout
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login");
+        }
+    }
+}
