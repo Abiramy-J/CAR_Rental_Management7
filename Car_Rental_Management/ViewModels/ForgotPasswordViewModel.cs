506410bc91cba20d64c9b56e0c78a69dfbdce284
UT010445 <kanga8482@gmail.com>
2025-09-16 15:55:14 +0530

    Forgot

---
+++ Car_Rental_Management/ViewModels/ForgotPasswordViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Car_Rental_Management.ViewModels
+{
+    public class ForgotPasswordViewModel : IValidatableObject
+    {
+        public string Username { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "New Password is required")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
+        public string NewPassword { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            // At least username or email must be provided
+            if (string.IsNullOrWhiteSpace(Username) && string.IsNullOrWhiteSpace(Email))
+            {
+                yield return new ValidationResult(
+                    "Please enter either Username or Email.",
+                    new[] { nameof(Username), nameof(Email) });
+            }
+        }
+    }
+}
